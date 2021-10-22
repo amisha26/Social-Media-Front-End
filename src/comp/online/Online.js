b1a98a43d0794b5b3ef5bc293213a58fcf6b87d0
amisha26 <amisha8299@gmail.com>
2021-10-22 23:15:41 +0530

    Online.js

--- src/comp/online/Online.js
+++ src/comp/online/Online.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import "./Online.css";
 
-const Online = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function Online({user}) {
+  return (
+    <li className="rightbarFriend">
+      <div className="rightbarProfileImgContainer">
+        <img className="rightbarProfileImg" src={user.profilePicture} alt="" />
+        <span className="rightbarOnline"></span>
+      </div>
+      <span className="rightbarUsername">{user.username}</span>
+    </li>
+  );
 }
-
-export default Online
