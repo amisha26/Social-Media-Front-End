be8820cda757d03a10a0497f3ce1c51788ffbdee
amisha26 <amisha8299@gmail.com>
2021-10-23 00:08:28 +0530

    Profile.js

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../comp/sidebar/Sidebar";
+import Feed from "../../comp/feed/Feed";
+import Rightbar from "../../comp/rightbar/Rightbar";
 
-const Profile = () => {
-    return (
-        <div>
-            
+export default function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/7.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+                <h4 className="profileInfoName">Safak Kocaoglu</h4>
+                <span className="profileInfoDesc">Hello my friends!</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile/>
+          </div>
         </div>
-    )
+      </div>
+    </>
+  );
 }
-
-export default Profile
