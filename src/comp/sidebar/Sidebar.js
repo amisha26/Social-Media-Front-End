dc7112392ac4f13ac3a34a0b574642cf8878e67e
amisha26 <amisha8299@gmail.com>
2021-10-22 23:42:26 +0530

    Sidebar done

--- src/comp/sidebar/Sidebar.js
+++ src/comp/sidebar/Sidebar.js
@@ -11,7 +11,7 @@ import {
   School,
 } from "@material-ui/icons";
 import { Users } from "../../dummyData";
-//import CloseFriend from "../closeFriend/CloseFriend";
+import CloseFriend from "../closeFriend/CloseFriend";
 
 export default function Sidebar() {
   return (
@@ -58,9 +58,9 @@ export default function Sidebar() {
         <button className="sidebarButton">Show More</button>
         <hr className="sidebarHr" />
         <ul className="sidebarFriendList">
-          {/* {Users.map((u) => (
+          {Users.map((u) => (
             <CloseFriend key={u.id} user={u} />
-          ))} */}
+          ))}
         </ul>
       </div>
     </div>
